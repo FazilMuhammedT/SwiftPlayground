4ca134d09c7938f83fc56a822e99ab79c85b9e1b
Fazil T <fazilt@gmail.com>
2019-05-14 18:58:34 -0500

    Comments, Semicolons and Integers

---
+++ SwiftProgramming.playground/Pages/Basic Operators.xcplaygroundpage/Contents.swift
@@ -0,0 +1,13 @@
+//: [Previous](@previous)
+//: # [Basic Operators](https://docs.swift.org/swift-book/LanguageGuide/BasicOperators.html)
+//: An operator is a special symbol or phrase that you use to check, change, or combine values.
+//:
+//: ## Terminology
+//:
+//: Operators are unary, binary, or ternary:
+//:
+//: * **Unary operators** operate on a single target (such as -a). Unary prefix operators appear immediately before their target (such as !b), and unary postfix operators appear immediately after their target (such as c!).
+//: * **Binary operators** operate on two targets (such as 2 + 3) and are infix because they appear in between their two targets.
+//: * **Ternary operators** operate on three targets. Like C, Swift has only one ternary operator, the ternary conditional operator (a ? b : c).
+//:
+//: [Next](@next)
--- SwiftProgramming.playground/Pages/The Basics.xcplaygroundpage/Contents.swift
+++ SwiftProgramming.playground/Pages/The Basics.xcplaygroundpage/Contents.swift
@@ -33,3 +33,32 @@ let languageName = "Swift"
 print(friendlyWelcome) //Prints "Bonjour!"
 //: Swift uses **string interpolation** to include the name of a constant or variable as a placeholder in a longer string, and to prompt Swift to replace it with the current value of that constant or variable. Wrap the name in parentheses and escape it with a backslash before the opening parenthesis:
 print("The current value of friendlyWelcome is \(friendlyWelcome)") // Prints "The current value of friendlyWelcome is Bonjour!"
+//:
+//: ## Comments
+//: Comments to include nonexecutable text in your code, as a note or reminder to yourself. Comments are ignored by the Swift compiler when your code is compiled.
+// This is a comment.
+
+/* This is also a comment
+ but is written over multiple lines. */
+
+/* This is the start of the first multiline comment.
+ /* This is the second, nested multiline comment. */
+ This is the end of the first multiline comment. */
+//:
+//: ## Semicolons
+//: Swift doesn’t require you to write a semicolon (;) after each statement in your code, although you can do so if you wish
+let cat = "🐱"; print(cat)// Prints "🐱"
+//:
+//: ## Integers
+//: Integers are whole numbers with no fractional component, such as 42 and -23. Integers are either signed (positive, zero, or negative) or unsigned (positive or zero).
+//:
+//: ### Integer Bounds
+let minValue = UInt8.min  // minValue is equal to 0, and is of type UInt8
+let maxValue = UInt8.max  // maxValue is equal to 255, and is of type UInt8
+//:
+//: ### Int
+//: * On a 32-bit platform, Int is the same size as Int32.
+//: * On a 64-bit platform, Int is the same size as Int64.
+//:
+//: On 32-bit platforms, Int can store any value between -2,147,483,648 and 2,147,483,647, and is large enough for many integer ranges.
+
