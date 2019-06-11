5acc2f21a0be0f4b7e151a49fc1c68601a1d5822
Fazil T <fazilt@gmail.com>
2019-06-11 16:29:38 -0500

    Basic Operators - Minor Modification

--- SwiftProgramming.playground/Pages/Basic Operators.xcplaygroundpage/Contents.swift
+++ SwiftProgramming.playground/Pages/Basic Operators.xcplaygroundpage/Contents.swift
@@ -1,8 +1,8 @@
-//: [Previous](@previous)
+//: [< The Basics](@previous)
 //: # [Basic Operators](https://docs.swift.org/swift-book/LanguageGuide/BasicOperators.html)
 //: An operator is a special symbol or phrase that you use to check, change, or combine values.
 //:
-//: ## [Terminology](https://docs.swift.org/swift-book/LanguageGuide/BasicOperators.html#ID61)
+//: ## Terminology
 //:
 //: Operators are unary, binary, or ternary:
 //:
@@ -12,46 +12,45 @@
 //:
 //: The values that operators affect are *operands*. In the expression `1 + 2`, the `+` symbol is a binary operator and its two operands are the values `1` and `2`.
 //:
-//: ## [Assignment Operator](https://docs.swift.org/swift-book/LanguageGuide/BasicOperators.html#ID62)
-//: The assignment operator `(a = b)` initializes or updates the value of `a` with the value of `b`:
+//: ## Assignment Operator
+//: The *assignment operator* `(a = b)` initializes or updates the value of `a` with the value of `b`:
 let b = 10
 var a = 5
 a = b // a is now equal to 10
-//:
 //: If the right side of the assignment is a tuple with multiple values, its elements can be decomposed into multiple constants or variables at once:
 let (x, y) = (1, 2) // x is equal to 1, and y is equal to 2
-//:
 //: Unlike the assignment operator in C and Objective-C, the assignment operator in Swift does not itself return a value. The following statement is not valid:
 /*
     if x = y {
     // This is not valid, because x = y does not return a value.
 }
  */
-// This feature prevents the assignment operator (`=`) from being used by accident when the equal to operator (`==`) is actually intended. By making if `x = y` invalid, Swift helps you to avoid these kinds of errors in your code.
+//: This feature prevents the assignment operator (`=`) from being used by accident when the equal to operator (`==`) is actually intended. By making if `x = y` invalid, Swift helps you to avoid these kinds of errors in your code.
 //:
-//: ## [Arithmetic Operators](https://docs.swift.org/swift-book/LanguageGuide/BasicOperators.html#ID63)
-//: Swift supports the four standard arithmetic operators for all number types:
+//: ## Arithmetic Operators
+//: Swift supports the four standard *arithmetic operators* for all number types:
 //: * Addition (`+`)
 //: * Subtraction (`-`)
-//: * Multiplication (`*`)
+//: *  Multiplication (`*`)
 //: * Division (`/`)
 1 + 2       // equals 3
 5 - 3       // equals 2
 2 * 3       // equals 6
 10.0 / 2.5  // equals 4.0
-//:
-//: Unlike the arithmetic operators in C and Objective-C, the Swift arithmetic operators don’t allow values to overflow by default. You can opt in to value overflow behavior by using Swift’s overflow operators (such as `a &+ b`). See [Overflow Operators](https://docs.swift.org/swift-book/LanguageGuide/AdvancedOperators.html#ID37).
-//:
+//: Unlike the arithmetic operators in C and Objective-C, the Swift arithmetic operators don’t allow values to overflow by default. You can opt in to value overflow behavior by using Swift’s overflow operators (such as `a &+ b`). See [Overflow Operators](https://docs.swift.org/swift-book/LanguageGuide/AdvancedOperators.html#ID37). \
 //: The addition operator is also supported for `String` concatenation:
 "hello, " + "world"  // equals "hello, world"
 //:
 //: ### Remainder Operator
 //: The remainder operator (`a % b`) works out how many multiples of b will fit inside a and returns the value that is left over (known as the *remainder*).
-//:
+//: - NOTE:
 //: The remainder operator (`%`) is also known as a *modulo operator* in other languages. However, its behavior in Swift for negative numbers means that, strictly speaking, it’s a remainder rather than a modulo operation.
 //:
-//: You can fit two 4s inside 9, and the remainder is 1. In Swift, this would be written as:
+//: Here’s how the remainder operator works. To calculate 9 % 4, you first work out how many 4s will fit inside 9:
 //:
+//: ![Remainder Integer](remainderInteger_2x.png)
+//:
+//: You can fit two 4s inside 9, and the remainder is 1. In Swift, this would be written as:
 9 % 4    // equals 1
 //: To determine the answer for `a % b`, the `%` operator calculates the following equation and returns remainder as its output:
 //:
@@ -59,13 +58,13 @@ let (x, y) = (1, 2) // x is equal to 1, and y is equal to 2
 //:
 //: where some `multiplier` is the largest number of multiples of `b` that will fit inside `a`.
 //:
-//: Inserting 9 and 4 into this equation yields:
+//: Inserting `9` and `4` into this equation yields:
 //:
 //: `9 = (4 x 2) + 1`
 //:
 //: The same method is applied when calculating the remainder for a negative value of `a`:
 -9 % 4   // equals -1
-//: Inserting -9 and 4 into the equation yields:
+//: Inserting `-9` and `4` into the equation yields:
 //:
 //: `-9 = (4 x -2) + -1`
 //:
@@ -82,7 +81,6 @@ let plusThree = -minusThree   // plusThree equals 3, or "minus minus three"
 //:
 //: ### Unary Plus Operator
 //: The *unary plus operator* (`+`) simply returns the value it operates on, without any change:
-//:
 let minusSix = -6
 let alsoMinusSix = +minusSix  // alsoMinusSix equals -6
 //: Although the unary plus operator doesn’t actually do anything, you can use it to provide symmetry in your code for positive numbers when also using the unary minus operator for negative numbers.
@@ -92,16 +90,18 @@ let alsoMinusSix = +minusSix  // alsoMinusSix equals -6
 var c = 1
 c += 2      // c is now equal to 3
 //: The expression `c += 2` is shorthand for `c = c + 2`. Effectively, the addition and the assignment are combined into one operator that performs both tasks at the same time.
+//: - NOTE:
+//: The compound assignment operators don’t return a value. For example, you can’t write `let b = a += 2`.
 //:
 //: ## Comparison Operators
-//: Swift supports all standard C comparison operators:
+//: Swift supports all standard C *comparison operators*:
 //: * Equal to (`a == b`)
 //: * Not equal to (`a != b`)
 //: * Greater than (`a > b`)
 //: * Less than (`a < b`)
 //: * Greater than or equal to (`a >= b`)
 //: * Less than or equal to (`a <= b`)
-//:
+//: - NOTE:
 //: Swift also provides two identity operators (`===` and `!==`), which you use to test whether two object references both refer to the same object instance. For more information, see [Identity Operators](https://docs.swift.org/swift-book/LanguageGuide/ClassesAndStructures.html#ID90).
 //:
 //: Each of the comparison operators returns a `Bool` value to indicate whether or not the statement is true:
@@ -111,30 +111,34 @@ c += 2      // c is now equal to 3
 1 < 2    // true because 1 is less than 2
 1 >= 1   // true because 1 is greater than or equal to 1
 2 <= 1   // false because 2 is not less than or equal to 1
-//: Comparison operators are often used in conditional statements, such as the if statement:
+//: Comparison operators are often used in conditional statements, such as the `if` statement:
 let name = "world"
 if name == "world" {
     print("hello, world")
 } else {
     print("I'm sorry \(name), but I don't recognize you")
 }  // Prints "hello, world", because name is indeed equal to "world".
-//:
 //: You can compare two tuples if they have the same type and the same number of values. Tuples are compared from left to right, one value at a time, until the comparison finds two values that aren’t equal. Those two values are compared, and the result of that comparison determines the overall result of the tuple comparison. If all the elements are equal, then the tuples themselves are equal. For example:
 (1, "zebra") < (2, "apple")   // true because 1 is less than 2; "zebra" and "apple" are not compared
 (3, "apple") < (3, "bird")    // true because 3 is equal to 3, and "apple" is less than "bird"
 (4, "dog") == (4, "dog")      // true because 4 is equal to 4, and "dog" is equal to "dog"
 ("blue", -1) < ("purple", 1)        // OK, evaluates to true
 //("blue", false) < ("purple", true)  // Error because < can't compare Boolean values
+//: - NOTE:
 //: The Swift standard library includes tuple comparison operators for tuples with fewer than seven elements. To compare tuples with seven or more elements, you must implement the comparison operators yourself.
 //:
-//: ##Ternary Conditional Operator
-//: The *ternary conditional operator* is a special operator with three parts, which takes the form `question ? answer1 : answer2`. It’s a shortcut for evaluating one of two expressions based on whether `question` is `true` or `false`. If `question` is true, it evaluates `answer1` and returns its value; otherwise, it evaluates `answer2` and returns its value.
+//: ## Ternary Conditional Operator
+//: The *ternary conditional operator* is a special operator with three parts, which takes the form `question ? answer1 : answer2`. It’s a shortcut for evaluating one of two expressions based on whether `question` is `true` or `false`. If `question` is true, it evaluates `answer1` and returns its value; otherwise, it evaluates `answer2` and returns its value. \
 //: The ternary conditional operator is shorthand for the code below:
-//: `if question {
-//:    answer1
-//: } else {
-//:    answer2
-//: }`
+/**
+ 
+ if question {
+    answer1
+ else {
+    answer2
+ }
+ 
+*/
 let contentHeight = 40
 let hasHeader = true
 var rowHeight = contentHeight + (hasHeader ? 50 : 20) // rowHeight is equal to 90
@@ -153,6 +157,7 @@ if hasHeader {
 var d:Int?
 let e = 1
 d != nil ? d! : e
+//: - NOTE:
 //: If the value of `d` is non-nil, the value of `e` is not evaluated. This is known as *short-circuit evaluation*.
 let defaultColorName = "red"
 var userDefinedColorName: String?   // defaults to nil
@@ -166,8 +171,7 @@ colorNameToUse = userDefinedColorName ?? defaultColorName
 //: ## Range Operators
 //: Swift includes several *range operators*, which are shortcuts for expressing a range of values.
 //: ### Closed Range Operator
-//: The *closed range operator* (`a...b`) defines a range that runs from `a to b`, and includes the values `a` and `b`. The value of a must not be greater than b.
-//:
+//: The *closed range operator* (`a...b`) defines a range that runs from `a to b`, and includes the values `a` and `b`. The value of a must not be greater than b. \
 //: The closed range operator is useful when iterating over a range in which you want all of the values to be used, such as with a for-in loop:
 for index in 1...5 {
     print("\(index) times 5 is \(index * 5)")
@@ -179,8 +183,7 @@ for index in 1...5 {
 // 5 times 5 is 25
 //:
 //: ### Half-Open Range Operator
-//: The *half-open range operator* (`a..<b`) defines a range that runs from `a` to `b`, but doesn’t include `b`. It’s said to be half-open because it contains its first value, but not its final value. As with the closed range operator, the value of a must not be greater than b. If the value of a is equal to b, then the resulting range will be empty.
-//:
+//: The *half-open range operator* (`a..<b`) defines a range that runs from `a` to `b`, but doesn’t include `b`. It’s said to be half-open because it contains its first value, but not its final value. As with the closed range operator, the value of a must not be greater than b. If the value of a is equal to b, then the resulting range will be empty. \
 //: Half-open ranges are particularly useful when you work with zero-based lists such as arrays, where it’s useful to count up to (but not including) the length of the list:
 let names = ["Anna", "Alex", "Brian", "Jack"]
 let count = names.count
@@ -224,6 +227,7 @@ range.contains(-1)  // true
 //: * Logical OR (`a || b`)
 //: ### Logical NOT Operator
 //: The *logical NOT* operator (`!a`) inverts a Boolean value so that `true` becomes `false`, and `false` becomes `true`.
+//: The logical NOT operator is a prefix operator, and appears immediately before the value it operates on, without any white space. It can be read as “not a”, as seen in the following example:
 let allowedEntry = false
 if !allowedEntry {
     print("ACCESS DENIED")
@@ -231,10 +235,8 @@ if !allowedEntry {
 //: The phrase if !allowedEntry can be read as “if not allowed entry.”
 //:
 //: ### Logical AND Operator
-// The *logical AND operator* (`a && b`) creates logical expressions where both values must be `true` for the overall expression to also be `true`.
-//:
-//: If either value is `false`, the overall expression will also be `false`. In fact, if the first value is `false`, the second value won’t even be evaluated, because it can’t possibly make the overall expression equate to `true`. This is known as *short-circuit evaluation*.
-//:
+// The *logical AND operator* (`a && b`) creates logical expressions where both values must be `true` for the overall expression to also be `true`.\
+//: If either value is `false`, the overall expression will also be `false`. In fact, if the first value is `false`, the second value won’t even be evaluated, because it can’t possibly make the overall expression equate to `true`. This is known as *short-circuit evaluation*. \
 //: This example considers two `Bool` values and only allows access if both values are `true`:
 let enteredDoorCode = true
 let passedRetinaScan = false
@@ -245,8 +247,7 @@ if enteredDoorCode && passedRetinaScan {
 } // Prints "ACCESS DENIED"
 //:
 //: ### Logical OR Operator
-//: The *logical OR operator* (`a || b`) is an infix operator made from two adjacent pipe characters. You use it to create logical expressions in which only one of the two values has to be `true` for the overall expression to be `true`.
-//:
+//: The *logical OR operator* (`a || b`) is an infix operator made from two adjacent pipe characters. You use it to create logical expressions in which only one of the two values has to be `true` for the overall expression to be `true`. \
 //: Like the Logical AND operator above, the Logical OR operator uses short-circuit evaluation to consider its expressions. If the left side of a Logical OR expression is `true`, the right side is not evaluated, because it can’t change the outcome of the overall expression.
 let hasDoorKey = false
 let knowsOverridePassword = true
@@ -263,6 +264,7 @@ if enteredDoorCode && passedRetinaScan || hasDoorKey || knowsOverridePassword {
 } else {
     print("ACCESS DENIED")
 } // Prints "Welcome!"
+//: - NOTE:
 //: The Swift logical operators `&&` and `||` are left-associative, meaning that compound expressions with multiple logical operators evaluate the leftmost subexpression first.
 //:
 //: Explicit Parentheses
@@ -272,6 +274,5 @@ if (enteredDoorCode && passedRetinaScan) || hasDoorKey || knowsOverridePassword 
 } else {
     print("ACCESS DENIED")
 } // Prints "Welcome!"
-//: Readability is always preferred over brevity; use parentheses where they help to make your intentions clear.
-//:
-//: [Next](@next)
+//: Readability is always preferred over brevity; use parentheses where they help to make your intentions clear. \
+//: [Strings and Characters >](@next)
